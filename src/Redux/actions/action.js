8aa7d14780ea63e1056cb0667fea82b9755f67b3
Dayaldonadkar <Dayalpython.com>
2023-09-11 16:45:46 +0530

    action added

---
+++ src/Redux/actions/action.js
@@ -0,0 +1,26 @@
+// export const ADD_CART = (item) => {
+//   return {
+//     type: "ADD_CART",
+//     payload: item,
+//   };
+// };
+
+// export const ADD_CART = (item) => {
+//   return {
+//     type: "ADD_CART",
+//     payload: item,
+//   };
+// };
+// export const ADD_CART = (item) => {
+//   return {
+//     type: "ADD_CART",
+//     payload: item,
+//   };
+// };
+
+export const ADD_ITEM = (item) => {
+  return {
+    type: "ADD_ITEM",
+    payload: item,
+  };
+};
